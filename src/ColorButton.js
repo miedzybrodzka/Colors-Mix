f8a4ffeb5d3714d33f04f7d73ef026d48e1d8673
miedzybrodzka.ela <miedzybrodzka.ela@gmail.com>
2021-10-17 18:04:38 +0200

    test

--- src/ColorButton.js
+++ src/ColorButton.js
@@ -6,6 +6,7 @@ export class ColorButton extends Component{
     render(){
         let view =  this.props.check? 'show' : 'hide';
             
+        
         return(
             <button onClick={this.props.function} className={this.props.className}
              id={this.props.id} disabled={this.props.disabled || this.props.check}><img src={check} 
